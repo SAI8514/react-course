073a15ab26793b607615534dc6af409ef599f793
SAI8514 <perisetlasaiganesh@gmail.com>
2023-09-15 09:56:30 +0530

    01basicvite is changed

--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -3,8 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
+
+function myApp(){
+    return(
+      <div>
+        <h1>Custom React App</h1>
+      </div>
+    )
+  }
+
+const reactElement = React.createElement(
+    'a',
+    {href: "https://google.com", target:"_blank"},
+    'Click to visit Google'
+)
+
+  const AnotherElement = (
+    <a href="http://google.com" target='_blank'> Visit Google</a>
+  )
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-    <App />
-
+   reactElement
 )
